b552267891708a530ad7d703e4c98bbb7eacc425
Ian Travers <mr.itpe@gmail.com>
2019-01-15 16:35:11 +0300

    Create accept answer Vue.js component

--- app/Answer.php
+++ app/Answer.php
@@ -28,7 +28,7 @@ class Answer extends Model
     ];
 
     protected $appends = [
-        'created_date', 'body_html',
+        'created_date', 'body_html', 'is_best'
     ];
 
     public function question()

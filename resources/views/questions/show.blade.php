734d23c8e05d8bd0a75dfabd934c1f1470104d78
Ian Travers <mr.itpe@gmail.com>
2019-01-16 16:04:21 +0300

    Create Answers component and implement Vue list rendering

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -42,10 +42,7 @@
             </div>
         </div>
 
-        @include('answers._index', [
-            'answersCount' => $question->answers_count,
-            'answers' => $question->answers,
-        ])
+        <answers :answers="{{ $question->answers }}" :count="{{ $question->answers_count }}"></answers>
 
         @include('answers._create')
     </div>
